e1b8a05e883ea4f41a8ab615c130fedf54bd9e09
Akhilesh Airen <akhilesh@summence.com>
2026-03-11 14:12:26 +0530

    added service worket to set COOP AND COEP headers

--- src/page_assets/index/js/main.js
+++ src/page_assets/index/js/main.js
@@ -4,5 +4,17 @@ import '../../../scss/styles.scss';
 
 import { init } from '../../../common/js/scl-app';
 
+async function registerCoiServiceWorker() {
+	if (!('serviceWorker' in navigator)) return;
+
+	try {
+		await navigator.serviceWorker.register('/sw-coi.js', { scope: '/' });
+		await navigator.serviceWorker.ready;
+	} catch {
+		// Ignore registration failures; app can still run without SW header shim.
+	}
+}
+
 // Start the app
+registerCoiServiceWorker();
 init();
